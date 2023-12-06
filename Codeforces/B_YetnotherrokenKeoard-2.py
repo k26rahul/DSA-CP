47613137e526261f6b1cf92406553a1d8f0d5546
Rahul Maurya <78677484+k26rahul@users.noreply.github.com>
2023-12-06 09:45:58 +0530

    B - YetnotherrokenKeoard

--- Codeforces/B_YetnotherrokenKeoard-2.py
+++ Codeforces/B_YetnotherrokenKeoard-2.py
@@ -7,24 +7,30 @@ for _ in range(T):
   n = len(word)
 
   final = ''
-  i_last_up = None
-  i_last_low = None
+  i_last_up = []
+  i_last_low = []
 
   for letter in word:
     if letter == 'b':
       if i_last_low:
-        final = final[:i_last_low] + final[i_last_low+1:]
+        i = i_last_low.pop()
+        final = final[:i] + final[i+1:]
+        i_last_up = [k - 1 if k > i else k for k in i_last_up]
+        i_last_low = [k - 1 if k > i else k for k in i_last_low]
 
     elif letter == 'B':
       if i_last_up:
-        final = final[:i_last_up] + final[i_last_up+1:]
+        i = i_last_up.pop()
+        final = final[:i] + final[i+1:]
+        i_last_up = [k - 1 if k > i else k for k in i_last_up]
+        i_last_low = [k - 1 if k > i else k for k in i_last_low]
 
     elif letter in ascii_lowercase:
       final += letter
-      i_last_low = len(final) - 1
+      i_last_low.append(len(final) - 1)
 
     elif letter in ascii_uppercase:
       final += letter
-      i_last_up = len(final) - 1
+      i_last_up.append(len(final) - 1)
 
-  print(word)
+  print(final)
